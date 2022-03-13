341edcb82b16bf8f93b52916e93c64628ea061ec
maximmnzh <maxim.mnzh@gmail.com>
2022-03-13 15:46:16 +0300

    added checks

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 
@@ -9,9 +11,9 @@ def go_to_login_page(browser):
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -19,3 +21,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_guest_should_see_login_page_forms(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
